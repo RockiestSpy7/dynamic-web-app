4dc987b603d8596a32a5cafb8ec601077bc9ac89
RockiestSpy7 <Cobyr159@gmail.com>
2023-12-08 11:52:49 -0500

    added nat-gateway module

--- main.tf
+++ main.tf
@@ -21,3 +21,19 @@ module "vpc" {
   private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
   private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
 }
+
+# create nat-gateway module
+module "nat-gateway" {
+  source = "git@github.com:RockiestSpy7/terraform-modules.git//nat-gateway"
+  # nat-gateway variables
+  project_name               = local.project_name
+  environment                = local.environment
+  public_subnet_az1_id       = module.vpc.public_subnet_az1_id
+  internet_gateway           = module.vpc.internet_gateway
+  public_subnet_az2_id       = module.vpc.public_subnet_az2_id
+  vpc_id                     = module.vpc.vpc_id
+  private_app_subnet_az1_id  = module.vpc.private_app_subnet_az1_id
+  private_data_subnet_az1_id = module.vpc.private_data_subnet_az1_id
+  private_app_subnet_az2_id  = module.vpc.private_app_subnet_az2_id
+  private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id
+}
