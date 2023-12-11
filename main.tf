bd9669634d0c603f290909929d8a3dbd8cc20e98
RockiestSpy7 <Cobyr159@gmail.com>
2023-12-11 15:30:10 -0500

    added ecs module

--- main.tf
+++ main.tf
@@ -103,3 +103,21 @@ module "ecs_task_execution_role" {
   env_file_bucket_name = module.s3_bucket.env_file_bucket_name
   environment          = local.environment
 }
+
+# create ecs cluster, task definition and service
+module "ecs" {
+  source = "git@github.com:RockiestSpy7/terraform-modules.git//ecs"
+  # ecs cluster variables
+  project_name                 = local.project_name
+  environment                  = local.environment
+  ecs_task_execution_role_arn  = module.ecs_task_execution_role.ecs_task_execution_role_arn
+  architecture                 = var.architecture
+  container_image              = var.container_image
+  env_file_bucket_name         = module.s3_bucket.env_file_bucket_name
+  env_file_name                = module.s3_bucket.env_file_name
+  region                       = local.region
+  private_app_subnet_az1_id    = module.vpc.private_app_subnet_az1_id
+  private_app_subnet_az2_id    = module.vpc.private_app_subnet_az2_id
+  app_server_security_group_id = module.security-groups.app_server_security_group_id
+  alb_target_group_arn         = module.application_load_balancer.alb_target_group_arn
+}
