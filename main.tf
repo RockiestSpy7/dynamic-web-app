db417aaa91b6618a1aea80675a8f1328f1db001a
RockiestSpy7 <Cobyr159@gmail.com>
2023-12-12 10:47:03 -0500

    Fixed alb misconfiguration

--- main.tf
+++ main.tf
@@ -76,14 +76,14 @@ module "ssl_certificate" {
 module "application_load_balancer" {
   source = "git@github.com:RockiestSpy7/terraform-modules.git//alb"
   # alb variables
-  project_name              = local.project_name
-  environment               = local.environment
-  alb_security_group_id     = module.security-groups.alb_security_group_id
-  private_app_subnet_az1_id = module.vpc.private_app_subnet_az1_id
-  private_app_subnet_az2_id = module.vpc.private_app_subnet_az2_id
-  target_type               = var.target_type
-  vpc_id                    = module.vpc.vpc_id
-  certificate_arn           = module.ssl_certificate.certificate_arn
+  project_name          = local.project_name
+  environment           = local.environment
+  alb_security_group_id = module.security-groups.alb_security_group_id
+  public_subnet_az1_id  = module.vpc.public_subnet_az1_id
+  public_subnet_az2_id  = module.vpc.public_subnet_az2_id
+  target_type           = var.target_type
+  vpc_id                = module.vpc.vpc_id
+  certificate_arn       = module.ssl_certificate.certificate_arn
 }
 
 # create s3 bucket module
